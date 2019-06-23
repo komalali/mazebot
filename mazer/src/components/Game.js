53fb99d9176d2297c7131cf31e8e86d59835b574
Komal Ali <komal@textio.com>
2019-06-23 00:20:22 -0700

    Moving around in the maze works!

--- mazer/src/components/Game.js
+++ mazer/src/components/Game.js
@@ -6,5 +6,5 @@ export default function Game() {
     const data = useFetch(
         'https://api.noopschallenge.com/mazebot/random?minSize=10&maxSize=10',
     );
-    return <Maze map={data.map} />;
+    return <Maze map={data.map} start={data.startingPosition} />;
 }
--- mazer/src/components/Maze.js
+++ mazer/src/components/Maze.js
@@ -1,16 +1,94 @@
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
+import useKey from 'react-use/lib/useKey';
 import Grid from './Grid';
 
-export default class Maze extends Component {
-    state = {
-        current: this.props.start,
-        path: [this.props.start],
-        map: this.props.map,
+const mazeReducer = (state, action) => {
+    const directionMap = {
+        moveRight: {
+            code: 'E',
+            position: [1, 0],
+        },
+        moveLeft: {
+            code: 'W',
+            position: [-1, 0],
+        },
+        moveUp: {
+            code: 'N',
+            position: [0, -1],
+        },
+        moveDown: {
+            code: 'S',
+            position: [0, 1],
+        },
+    };
+    const size = state.map.length;
+    switch (action.type) {
+        case 'moveRight':
+        case 'moveDown':
+        case 'moveLeft':
+        case 'moveUp':
+            const minIndex = 0;
+            const maxIndex = size - 1;
+            console.log(state);
+            const xValue =
+                state.position[0] + directionMap[action.type].position[0];
+            const yValue =
+                state.position[1] + directionMap[action.type].position[1];
+            if (xValue < minIndex || xValue > maxIndex) {
+                // throw new Error(`Invalid x value: ${xValue}`);
+                return state;
+            }
+            if (yValue < minIndex || yValue > maxIndex) {
+                // throw new Error(`Invalid y value: ${yValue}`);
+                return state;
+            }
+
+            const newPosition = [xValue, yValue];
+            const newMap = [...state.map];
+            const newCell = newMap[yValue][xValue];
+
+            if (newCell === 'X') {
+                // throw new Error('You crashed into a wall');
+                return state;
+            }
+
+            newMap[yValue][xValue] = ['A', 'B'].includes(newCell)
+                ? newCell
+                : 'A';
+
+            return {
+                map: newMap,
+                position: newPosition,
+                steps: state.steps + 1,
+                solution: `${state.solution}${directionMap[action.type].code}`,
+                solved: newCell === 'B',
+            };
+        default:
+            return;
+    }
+};
+
+const Maze = props => {
+    const initialState = {
+        map: props.map,
+        position: props.start,
+        steps: 0,
         solution: '',
         solved: false,
     };
 
-    render() {
-        return <Grid grid={this.state.map} />;
-    }
-}
+    const [state, dispatch] = useReducer(mazeReducer, initialState);
+
+    const up = () => dispatch({ type: 'moveUp' });
+    const down = () => dispatch({ type: 'moveDown' });
+    const right = () => dispatch({ type: 'moveRight' });
+    const left = () => dispatch({ type: 'moveLeft' });
+
+    useKey('ArrowUp', up);
+    useKey('ArrowDown', down);
+    useKey('ArrowRight', right);
+    useKey('ArrowLeft', left);
+    return <Grid grid={state.map} />;
+};
+
+export default Maze;
